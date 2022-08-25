943db2c3bc2fa1542eaed59803cded4b848672a4
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2022-08-25 11:28:59 +0200

    add MCLInference

--- costometer/inference/__init__.py
+++ costometer/inference/__init__.py
@@ -1,2 +1,3 @@
 from costometer.inference.grid import GridInference
+from costometer.inference.mcl import MCLInference
 from costometer.inference.ray_inference import GridRayInference, RayInferenceMCL
