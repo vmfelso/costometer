5364d934b44d43ffc52eec3980b3d343796c5bcf
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2022-08-25 11:31:45 +0200

    add KubalaInference (very rough, not finished / tested)

--- costometer/inference/__init__.py
+++ costometer/inference/__init__.py
@@ -1,3 +1,4 @@
 from costometer.inference.grid import GridInference
+from costometer.inference.kubala import KubalaInference
 from costometer.inference.mcl import MCLInference
 from costometer.inference.ray_inference import GridRayInference, RayInferenceMCL
