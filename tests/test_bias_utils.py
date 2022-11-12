2943a02e35eeff2952d49207ceb027b146c2e2c4
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2022-11-12 14:20:55 +0100

    test scaffolding

--- tests/test_bias_utils.py
+++ tests/test_bias_utils.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+import pandas as pd
 import pytest
 
 from costometer.utils.bias_utils import (  # noqa : F401
@@ -8,20 +11,15 @@ from costometer.utils.bias_utils import (  # noqa : F401
 
 
 # some setup where you load fake / simulate data as 'pytest fixture'
-@pytest.fixture(
-    params=[
-        # where you put the test case info (paths to files, etc)
-        [],
-        [],
-        [],
-    ]
-)
-def bias_fixutre(request):
-    # should yield outputs that will be used for testing biases
-    yield request
+@pytest.fixture()
+def bias_fixture():
+    mouselab_data = pd.read_csv(
+        Path(__file__).parents[0].joinpath("inputs/fake_data/mouselab_mdp.csv")
+    )
+    yield mouselab_data
 
 
-def test_calculate_overplanning(bias_fixutre):
+def test_calculate_overplanning(bias_fixture):
     return NotImplementedError
 
 
@@ -29,5 +27,5 @@ def test_calculate_present_bias(bias_fixture):
     return NotImplementedError
 
 
-def test_calculate_present_bias_late(bias_fixutre):
+def test_calculate_present_bias_late(bias_fixture):
     return NotImplementedError
