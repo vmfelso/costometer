7328fb1530f5aef718a967a95b487b7677019876
vmfelso <vmfelso@users.noreply.github.com>
2023-01-20 18:08:38 +0100

    remove Kubala, MCL in __init__.py

--- costometer/inference/__init__.py
+++ costometer/inference/__init__.py
@@ -1,4 +1,2 @@
 from costometer.inference.grid import GridInference
-from costometer.inference.kubala import KubalaInference
-from costometer.inference.mcl import MCLInference
-from costometer.inference.ray_inference import GridRayInference, RayInferenceMCL
+from costometer.inference.ray_inference import GridRayInference
