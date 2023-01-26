ccb6947fce88af1687c9ca34955be22eac022685
vmfelso <vmfelso@users.noreply.github.com>
2023-01-26 21:27:30 +0100

    remove optimization libraries from setup.py

--- setup.py
+++ setup.py
@@ -22,12 +22,10 @@ setup(
         "colorcet",
         "dill",
         "gym==0.21.0",  # unfortunately DiscreteEnv class was removed in 0.22.0: https://github.com/openai/gym/pull/2514 # noqa
-        "hyperopt",
         "mcl_toolbox @ git+https://github.com/RationalityEnhancementGroup/mcl_toolbox.git@dev#egg=mcl_toolbox",  # noqa
         "mouselab @ git+https://github.com/RationalityEnhancementGroup/mouselab-mdp-tools.git@dev#egg=mouselab"  # noqa
         "numpy",
         "pandas",
-        "ray[tune]",
         "statsmodels",
         "tqdm",
         "blosc",
